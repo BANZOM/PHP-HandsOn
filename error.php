d7a6eb32557ca578fd6ba089b9fd6b58ada70698
banzom <aditya.panwar.2208@gmail.com>
2024-06-08 20:17:30 +0530

    Add error.php to handle 404 page not found errors

---
+++ error.php
@@ -0,0 +1,8 @@
+<?php
+// Set the HTTP response code to 404
+http_response_code(404);
+
+// Display the error message
+echo "<h1>Error 404: Page Not Found</h1>";
+echo "<p>The requested page could not be found.</p>";
+?>
